574cd3334851293b9dbb547672320ea7334722ac
Barbara Imperatori <barbarabelen.i@gmail.com>
2022-03-07 12:46:02 -0300

    Set API url, render pokemon list, set next and previous page buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import React, { useState, useEffect } from "react";
+import PokemonList from "./components/PokemonList";
+import Pagination from "./components/Pagination";
+
+import axios from "axios";
+
 function App() {
-  return <div className="App"></div>;
+  const [pokemon, setPokemon] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [mainURL, setMainURL] = useState("https://pokeapi.co/api/v2/pokemon");
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(mainURL, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextURL(res.data.next);
+        setPrevURL(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+
+    //cancel the request when we make a new one
+    return () => cancel();
+  }, [mainURL]);
+
+  function nextPage() {
+    setMainURL(nextURL);
+  }
+
+  function prevPage() {
+    setMainURL(prevURL);
+  }
+
+  if (loading) return "Loading...";
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        nextPage={nextURL ? nextPage : null}
+        prevPage={prevURL ? prevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
